b4ddb64e27358800864cd405b18344aec24a5296
Aina <ainaeinikyte@gmail.com>
2024-03-29 12:31:01 +0200

    feat: Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+function calculateTip() {
+  const billElement = document.getElementById("bill");
+  const percentageTipElement = document.getElementById("percentageTip");
+
+  const bill = Math.abs(parseInt(billElement.value));
+  const tipPercentage = Math.abs(parseInt(percentageTipElement.value));
+
+  billElement.value = bill;
+  percentageTipElement.value = tipPercentage;
+
+  const totalTip = Math.floor(bill * tipPercentage) / 100;
+  const totalToPay = bill + totalTip;
+
+  document.getElementById("tipAmount").innerText = totalTip.toFixed(2) + "€";
+  document.getElementById("totalPay").innerText = totalToPay.toFixed(2) + "€";
+}
+
+function resetCalculator() {
+  document.getElementById("bill").value = "";
+  document.getElementById("percentageTip").value = "";
+  document.getElementById("tipAmount").innerText = "";
+  document.getElementById("totalPay").innerText = "";
+}
